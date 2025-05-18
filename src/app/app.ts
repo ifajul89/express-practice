499fc389e37abfc9b642eec3d05102124bbcafcf
Ifajul Islam <135067157+ifajul89@users.noreply.github.com>
2025-05-18 14:24:04 +0600

    Enhance Express server: add JSON and text parsers, implement POST endpoint, and rename main function to startServer for clarity.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,22 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+// parser
+app.use(express.json());
+app.use(express.text());
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Server is running ⚡️");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+  // res.send("Send Request");
+  res.json({
+    message: "Request sent",
+  });
 });
 
 export default app;
--- src/app/server.ts
+++ src/app/server.ts
@@ -5,10 +5,10 @@ const PORT = 5000;
 
 let server: Server;
 
-async function main() {
+async function startServer() {
   const server = app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`);
   });
 }
 
-main();
+startServer();
